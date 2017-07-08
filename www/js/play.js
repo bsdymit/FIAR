e13f3a4f235818691724c46b6d0ed425d134489f
bsdymit <brad.dymit@gmail.com>
2017-07-08 13:55:47 -0500

    Fixing board hidden issue

--- www/js/play.js
+++ www/js/play.js
@@ -32,7 +32,7 @@ var previousMoveDirection;
 var BOARDSTATE = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 $(function() {
-  $('#board').fadeIn(500);
+  $('#board').hide().fadeIn(1);
   $('#gameMenuOptionButtons').fadeIn(500);
 });
 
