48ed8b6ce50574834dbe1838b3fe7ead00919abc
bsdymit <brad.dymit@gmail.com>
2017-07-12 22:55:52 -0500

    Giving up. Splash screens are dumb

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('deviceready', function() {
-  setTimeout(function() {navigator.splashscreen.hide();}, 1000);
+  setTimeout(function() {navigator.splashscreen.hide();}, 1);
 });
 
 $(function() {
