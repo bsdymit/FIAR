91c6eae3c8ce7e691548bca0c0ab77f3adadbd8a
bsdymit <brad.dymit@gmail.com>
2017-07-17 17:35:20 -0500

    Removing permissions and increasing fade in time.

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ document.addEventListener('deviceready', function() {
 });
 
 $(function() {
-  $('#gameTitle').fadeIn(500,function() {
+  $('#gameTitle').fadeIn(1000,function() {
     $('#play').fadeIn(500,function() {
        $('#howToPlay').fadeIn(500,function() {
          $('#about').fadeIn(500)
