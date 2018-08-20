74e9ebf467263b2b0bae1dfec2b65a3224ddc8c7
Brad Dymit <brad.dymit@gmail.com>
2018-08-19 22:30:44 -0500

    adding more CPU decision before movement

--- www/js/cpuPlayer.js
+++ www/js/cpuPlayer.js
@@ -6,10 +6,14 @@ function easyMove() {
     var chosenEmptyTile = Math.random() < 0.5 ? 0 : 3;
     var locationOfChosenEmptyTile = BOARDSTATE.indexOf(chosenEmptyTile);
     var tileToMove = getTileToMove(locationOfChosenEmptyTile);
+	
+	makeMove(locationOfChosenEmptyTile, tileToMove);
 }
 
 function getTileToMove(locationOfChosenEmptyTile) {
-    
+    var listOfAdjTileLocs = getAdjacentTileLocations(locationOfChosenEmptyTile);
+	document.getElementById("poop").innerHTML = listOfAdjTileLocs;
+	return listOfAdjTileLocs[Math.floor(Math.random()*listOfAdjTileLocs.length)];
 }
 
 function getAdjacentTileLocations(locationOfChosenEmptyTile) {
@@ -17,7 +21,7 @@ function getAdjacentTileLocations(locationOfChosenEmptyTile) {
 
     if(locationOfChosenEmptyTile != 0 && locationOfChosenEmptyTile != 4
         && locationOfChosenEmptyTile != 8 && locationOfChosenEmptyTile != 12) {
-            listOfAdjacentTiles.push(locationOfChosenEmptyTile - 1)
+            listOfAdjacentTiles.push(locationOfChosenEmptyTile - 1);
     }
 
     if(locationOfChosenEmptyTile != 3 && locationOfChosenEmptyTile != 7
@@ -32,11 +36,27 @@ function getAdjacentTileLocations(locationOfChosenEmptyTile) {
 
     if(locationOfChosenEmptyTile != 12 && locationOfChosenEmptyTile != 13
         && locationOfChosenEmptyTile != 14 && locationOfChosenEmptyTile != 15) {
-            listOfAdjacentTiles.push(locationOfChosenEmptyTile - 4);
+            listOfAdjacentTiles.push(locationOfChosenEmptyTile + 4);
     }
 
-    for()
-
+    for(i = 0; i < listOfAdjacentTiles.length; i++) {
+		if(BOARDSTATE[listOfAdjacentTiles[i]] == 0 || BOARDSTATE[listOfAdjacentTiles[i]] == 3
+		|| lastMovedTile(BOARDSTATE[listOfAdjacentTiles[i]]))
+			listOfAdjacentTiles.splice(i, 1);
+	}
 
     return listOfAdjacentTiles;
+}
+
+function makeMove(emptyTileLoc, selectedTileLoc) {
+	var winningColor = checkForWinner();
+    if(winningColor == playerColors[0] || winningColor == playerColors[1])
+      endGame(playerColors.indexOf(winningColor));
+
+    if(lastMovedTileNumber != -1)
+      document.getElementById("pos" + BOARDSTATE[selectedTileLoc]).style.border = "";
+    setLastMovedTileNumber(BOARDSTATE[selectedTileLoc]);
+    document.getElementById("pos" + BOARDSTATE[selectedTileLoc]).style.border = "2px white dotted";
+
+    nextPlayer(isCpuGame);
 }
