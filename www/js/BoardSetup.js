eec6807cf3eb327b7febdeb30aba8c460fbd5c91
bsdymit <brad.dymit@gmail.com>
2018-09-01 17:23:28 -0500

    Getting further on ai menu

--- www/js/BoardSetup.js
+++ www/js/BoardSetup.js
@@ -9,14 +9,14 @@ window.onload = function setUpScreen() {
 
 function setPlayerEntryDialogBoxes() {
   isCpuGame = window.name;
-  if(isCpuGame.localeCompare("false") != 0) {
-	var colorSelectionDialog = document.getElementById( 'colorSelectionDialog'  );
-	var dlg2 = new DialogFx( colorSelectionDialog );
+  if (isCpuGame.localeCompare("false") != 0) {
+    var colorSelectionDialog = document.getElementById('colorSelectionDialog');
+    var dlg2 = new DialogFx(colorSelectionDialog);
     dlg2.toggle(dlg2);
   }
   else {
-    var playerEntryDialog = document.getElementById( 'playerEntryDialog'  );
-	var dlg1 = new DialogFx( playerEntryDialog );
+    var playerEntryDialog = document.getElementById('playerEntryDialog');
+    var dlg1 = new DialogFx(playerEntryDialog);
     dlg1.toggle(dlg1);
   }
 }
@@ -34,18 +34,18 @@ function setBoardAndTileSizes() {
   TILEWIDTH = document.getElementById("pos0").getBoundingClientRect().width;
 }
 
-function setSquareWidthAndMargins(squareWidth){
+function setSquareWidthAndMargins(squareWidth) {
   var elements = document.getElementsByClassName("square");
   for (var i = 0; i < 4; i++) {
-    for(var j = 0; j < 4; j++) {
-      elements[(4*i)+j].style.width = (squareWidth + "px");
-      elements[(4*i)+j].style.paddingBottom = (squareWidth + "px");
-      elements[(4*i)+j].style.left = (j*squareWidth) + ((j+1)*SQUAREMARGIN) + "px";
-      elements[(4*i)+j].style.top = (i*squareWidth) + ((i+1)*SQUAREMARGIN) + "px";
+    for (var j = 0; j < 4; j++) {
+      elements[(4 * i) + j].style.width = (squareWidth + "px");
+      elements[(4 * i) + j].style.paddingBottom = (squareWidth + "px");
+      elements[(4 * i) + j].style.left = (j * squareWidth) + ((j + 1) * SQUAREMARGIN) + "px";
+      elements[(4 * i) + j].style.top = (i * squareWidth) + ((i + 1) * SQUAREMARGIN) + "px";
     }
   }
 }
 
 var round = function (x, to) {
-    return Math.floor(x / to) * to;
+  return Math.floor(x / to) * to;
 };
--- www/js/Players.js
+++ www/js/Players.js
@@ -1,13 +1,13 @@
 var players = [];
 var currentPlayer;
+var playerChoseFirst;
 var playerColors = ["teal", "grey"];
 
 function enterPlayer() {
   var inputElement = document.getElementById("playerInput");
 
-  if(players.length == 0)
-  {
-    if(inputElement.value == "")
+  if (players.length == 0) {
+    if (inputElement.value == "")
       players.push("Teal");
     else
       players.push(inputElement.value);
@@ -23,11 +23,12 @@ function enterPlayer() {
     document.getElementById("enterPlayerOkay").setAttribute('data-dialog-close', '');
     document.getElementById("enterPlayerSkip").setAttribute('data-dialog-close', '');
 
-	toggleDialog("playerEntryDialog");
+    var playerEntryDialog = document.getElementById('playerEntryDialog');
+    var dlg = new DialogFx(playerEntryDialog);
+    dlg.toggle(dlg);
   }
-  else
-  {
-    if(inputElement.value == "")
+  else {
+    if (inputElement.value == "")
       players.push("Grey");
     else
       players.push(inputElement.value);
@@ -35,30 +36,55 @@ function enterPlayer() {
 }
 
 function playerChoseFirst() {
-	toggleDialog("playerEntryDialog");
-	toggleDialog("colorSelectionDialog");
-	var inputElement = document.getElementById("playerInput");
+  playerChoseFirst = true;
+  var colorSelectionDialog = document.getElementById('colorSelectionDialog');
+  var dlg1 = new DialogFx(colorSelectionDialog);
+  dlg1.toggle(dlg1);
 
-    if(inputElement.value == "")
+  document.getElementById("enterPlayerSkip").setAttribute('data-dialog-close', '');
+  document.getElementById("enterPlayerOkay").setAttribute('data-dialog-close', '');
+  document.getElementById("enterPlayerSkip").setAttribute('onClick', 'cpuGoesSecond()');
+  document.getElementById("enterPlayerOkay").setAttribute('onClick', 'cpuGoesSecond()');
+
+  var playerEntryDialog = document.getElementById('playerEntryDialog');
+  var dlg2 = new DialogFx(playerEntryDialog);
+  dlg2.toggle(dlg2);
+}
+
+function cpuGoesSecond() {
+  var inputElement = document.getElementById("playerInput");
+
+  if (players.length == 0) {
+    if (inputElement.value == "")
       players.push("Teal");
     else
       players.push(inputElement.value);
-
-    setCurrentPlayer(players[0]);
-    document.getElementById("currentPlayer").innerHTML = getCurrentPlayer();
-    document.getElementById("currentPlayer").style.backgroundColor = playerColors[0];
-	
+  }
 }
 
 function playerChoseSecond() {
-	
+  playerChoseFirst = "false";
+  players.push("CPU");
+  setCurrentPlayer(players[0]);
+
+  document.getElementById("enterPlayerSkip").setAttribute('data-dialog-close', '');
+  document.getElementById("enterPlayerOkay").setAttribute('data-dialog-close', '');
+  document.getElementById("enterPlayerSkip").setAttribute('onClick', 'cpuGoesFirst()');
+  document.getElementById("enterPlayerOkay").setAttribute('onClick', 'cpuGoesFirst()');
+  document.getElementById("playerEntryPrompt").innerHTML = "Grey";
+  document.getElementById("playerEntryPrompt").style.color = playerColors[1];
+
+  var colorSelectionDialog = document.getElementById('colorSelectionDialog');
+  var dlg1 = new DialogFx(colorSelectionDialog);
+  dlg1.toggle(dlg1);
+
+  var playerEntryDialog = document.getElementById('playerEntryDialog');
+  var dlg3 = new DialogFx(playerEntryDialog);
+  dlg3.toggle(dlg3);
 }
 
-function toggleDialog(dialogId)
-{
-	var playerEntryDialog = document.getElementById( dialogId );
-	var dlg = new DialogFx( playerEntryDialog );
-    dlg.toggle(dlg);
+function cpuGoesFirst() {
+
 }
 
 function setCurrentPlayer(currentPlayer) {
@@ -68,20 +94,20 @@ function setCurrentPlayer(currentPlayer) {
 }
 
 function getCurrentPlayer() {
- return currentPlayer;
+  return currentPlayer;
 }
 
 function nextPlayer(isCpuTurn) {
-	if(currentPlayer == players[0])
-		currentPlayer = players[1];
-	else
-    	currentPlayer = players[0];
+  if (currentPlayer == players[0])
+    currentPlayer = players[1];
+  else
+    currentPlayer = players[0];
 
-	document.getElementById("currentPlayer").innerHTML = currentPlayer;
-	document.getElementById("currentPlayer").style.backgroundColor = playerColors[players.indexOf(currentPlayer)];
+  document.getElementById("currentPlayer").innerHTML = currentPlayer;
+  document.getElementById("currentPlayer").style.backgroundColor = playerColors[players.indexOf(currentPlayer)];
 
- 	if(isCpuTurn.localeCompare("false") != 0) {
-		playCpuTurn(isCpuTurn);
-		nextPlayer(false);
-	}
+  if (isCpuTurn.localeCompare("false") != 0) {
+    playCpuTurn(isCpuTurn);
+    nextPlayer(false);
+  }
 }
